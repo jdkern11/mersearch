083db3681a5b2e3ec025ae24bb8e2577a375836e
jdkern11 <leolion29@gmail.com>
2023-03-01 08:50:01 -0500

    finish testing smiles to mol block

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from rdkit import Chem
 
 from mersearch import helpers
 
@@ -15,28 +16,10 @@ def test_smiles_to_mol_file_format(mol_file_loc):
     for f in files:
         smiles = f.name.split('.')[0]
         mol_from_smiles = helpers.smiles_to_mol_file_format(smiles)
+        m1 = Chem.rdmolfiles.MolFromMolBlock(mol_from_smiles)
         with open(f, 'r') as fin:
             mol_from_file = ''.join(fin.readlines())
-        mol_from_smiles = mol_from_smiles.split('\n')
-        mol_from_file = mol_from_file.split('\n')
-        num_atoms = 0
-        num_bonds = 0
-        for i in range(header_len + 1, len(mol_from_smiles) - 2):
-            s_line = ' '.join(mol_from_smiles[i].strip().split()).split()
-            f_line = ' '.join(mol_from_file[i].strip().split()).split()
-            if i == header_len + 1:
-                num_atoms = int(s_line[0])
-                num_bonds = int(s_line[1])
-                assert num_atoms == int(f_line[0])
-                assert num_bonds == int(f_line[1])
-            elif i < header_len + 1 + num_atoms + 1:
-                element = s_line[3]
-                assert element == f_line[3]
-            else:
-                print(s_line)
-                print(f_line)
-                s_bond = s_line[0:4]
-                f_bond = f_line[0:4]
-                s_bond.sort()
-                f_bond.sort()
-                assert s_bond == f_bond
+        m2 = Chem.rdmolfiles.MolFromMolBlock(mol_from_file)
+        m1 = Chem.MolToSmiles(m1)
+        m2 = Chem.MolToSmiles(m2)
+        assert Chem.CanonSmiles(m1) == Chem.CanonSmiles(m2)
