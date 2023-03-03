1caaf11fd780a40a8dfa817efd5ed5101663373d
jdkern11 <leolion29@gmail.com>
2023-03-03 11:05:22 -0500

    update with waits in place

--- mersearch/helpers.py
+++ mersearch/helpers.py
@@ -10,3 +10,15 @@ def smiles_to_mol_file_format(smiles: str) -> str:
     """
     mol = Chem.MolFromSmiles(smiles)
     return Chem.MolToMolBlock(mol)
+
+def scroll_shim(passed_in_driver, object):
+    """Allows firefox driver to scroll to elements without throwing an error"""
+    x = object.location['x']
+    y = object.location['y']
+    scroll_by_coord = 'window.scrollTo(%s,%s);' % (
+        x,
+        y
+    )
+    scroll_nav_out_of_way = 'window.scrollBy(0, -120);'
+    passed_in_driver.execute_script(scroll_by_coord)
+    passed_in_driver.execute_script(scroll_nav_out_of_way)
--- mersearch/vendors.py
+++ mersearch/vendors.py
@@ -1,14 +1,29 @@
-from typing import Dict
+from typing import Dict, List
 
-import selenium
+from bs4 import BeautifulSoup
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
 
-def search_vwr(mol: str) -> Dict[str, str]:
+def search_vwr(driver: WebDriver, smiles_list: List[str]) -> Dict[str, str]:
     """Search VWR International website for specific molecule
 
     Args:
-        mol: mol file format string to search for
+        driver: web driver to search for molecule using.
+        smiles_list: list of smiles string to search for
     Returns:
         Dictionary with data parsed from the VWR search. If no data found, dictionary
         will be empty.
     """
+    login_to_vwr(driver)
 
+def login_to_vwr(driver: WebDriver):
+    """Logs in to vwr by navigating to https://us.vwr.com/store/search/searchMol.jsp,
+    then continuing to https://vwr.emolecules.com/index.php from a button on the first
+    page
+
+    Args:
+        driver: WebDriver used for search
+    """
